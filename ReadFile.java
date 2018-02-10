b8b891fb11307143062ca5b6c70a44aec8b99e6e
iamrico <hinatashoyo1201@gmail.com>
2018-02-10 18:38:18 +0800

    Add files via upload

--- ReadFile.java
+++ ReadFile.java
@@ -8,32 +8,48 @@ package readfile;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import parser.Parser;
+import readfile.tokenizer.Token;
+import readfile.tokenizer.Tokenizer;
 /**
  *
  * @author User
  */
 public class ReadFile {
-
+   
     /**
      * @param args the command line arguments
      */
-    
+    public ReadFile(){
+         
+    }
     private static final String FILENAME = "C:\\Users\\User\\source.txt";
     public static void main(String[] args) {
         // TODO code application logic here
+        
         BufferedReader br = null;
 		FileReader fr = null;
-                
+                ArrayList<Token> tokGroup = new ArrayList<Token>();
                 try {
 
 			//br = new BufferedReader(new FileReader(FILENAME));
 			fr = new FileReader(FILENAME);
 			br = new BufferedReader(fr);
-
+                        
 			String sCurrentLine;
-
+                        int ctr=0;
 			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+                                Tokenizer tknObj = new Tokenizer(sCurrentLine);
+				System.out.println("\n Line"+ctr+":");
+                                while(tknObj.hasNextToken()){
+                                   Token retVal = tknObj.nextToken();
+                                   tokGroup.add(new Token(retVal.getToken(),retVal.getTokenType()));
+                                   //Parser parser =new Parser(tokGroup);
+                                   //parser.Start();
+                                   //System.out.println(retVal.getToken()+"=>"+retVal.getTokenType());
+                                }
+                                ctr++;
 			}
 
 		} catch (IOException e) {
