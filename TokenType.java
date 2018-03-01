d63b4138c6733055ff7af339b2cd443900c43d09
iamrico <hinatashoyo1201@gmail.com>
2018-03-01 10:19:13 +0800

    Add files via upload

--- TokenType.java
+++ TokenType.java
@@ -13,25 +13,23 @@ public enum TokenType {
     
     
     EMPTY,
-    
     /*A token. For example:(,),:,*,+,-,,,*/
     TOKEN,
-    
     /*A name for a variable. First character is a letter and any procedding characters are letter or numbers*/
     IDENTIFIER,
-    
     /*A word enclose with "" */
     STRING_LITERAL,
-    
     /*A number*/
     INTEGER_LITERAL,
-    
     /*A number with a decimal point*/
     FLOAT_LITERAL,
-    
     /*either word,words,number,flag* you gotta check for this*/
     DATA_TYPE,
-    
     /*a reserved word you gotta check for this*/
-    KEYWORD
+    KEYWORD,
+    
+    EXPRESSION,
+    
+    OPERATION,
+    
 }
