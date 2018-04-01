e2eaf546efe3c6d48bf21d8dd33a434d45df1726
bojoluis21798 <trinity21798@gmail.com>
2018-04-01 21:57:40 +0800

    Fixed rob's problem

--- readFile/src/readfile/ReadFile.java
+++ readFile/src/readfile/ReadFile.java
@@ -152,23 +152,23 @@ public class ReadFile {
 
                 }else if(program.get(ctr).getCode().get(0).getToken().equals("end")) {
 
-                    boolean found = false
-                    for (int x = ctr - 1; x >= 0 && found == false) {
+                    boolean found = false;
+                    for (int x = ctr - 1; x >= 0 && found == false;x--) {
 
-                        if (program.get(x).getType().equals.("IF STATEMENT!")
-                            || program.get(x).getType().equals.("ORIF STATEMENT!")
-                            || program.get(x).getType().equals.("ELSE STATEMENT!")) {
+                        if (program.get(x).getType().equals("IF STATEMENT!")
+                            || program.get(x).getType().equals("ORIF STATEMENT!")
+                            || program.get(x).getType().equals("ELSE STATEMENT!")) {
 
                             levelsAndLines.add(new tuple(program.get(ctr).getIndex(),level--));
                             found = true;
-                        } else if ((program.get(x).getType().equals.("PRE TEST LOOP!")) {
+                        } else if ((program.get(x).getType().equals("PRE TEST LOOP!"))) {
 
                             loopTracker.add(new tuple(program.get(ctr).getIndex(),level--));
                             found = true;
-                        } else if (program.get(x).getType().equals.("JOB DECLARATION!")
-                                    || program.get(x).getType().equals.("JOB DECLARATION WITHOUT PARAMS AND RETURN TYPE!")
-                                    || program.get(x).getType().equals.("JOB DECLARATION WITHOUT PARAMS!")
-                                    || program.get(x).getType().equals.("JOB DECLARATION WITHOUT RETURN TYPE!")) {
+                        } else if (program.get(x).getType().equals("JOB DECLARATION!")
+                                    || program.get(x).getType().equals("JOB DECLARATION WITHOUT PARAMS AND RETURN TYPE!")
+                                    || program.get(x).getType().equals("JOB DECLARATION WITHOUT PARAMS!")
+                                    || program.get(x).getType().equals("JOB DECLARATION WITHOUT RETURN TYPE!")) {
                             
                             functionTrav.add(new tuple(program.get(ctr).getIndex(),level--));
                             found = true;
@@ -182,11 +182,11 @@ public class ReadFile {
 
                 }else if (program.get(ctr).getCode().get(0).getToken().equals("do")) {
 
-                    loopTracker.add(new tuple(program.get(ctr).getIndex(), ++level))
+                    loopTracker.add(new tuple(program.get(ctr).getIndex(), ++level));
 
                 }else if (program.get(ctr).getType().equals("WHILE!")) {
 
-                    loopTracker.add(new tuple(program.get(ctr).getIndex(), level--))
+                    loopTracker.add(new tuple(program.get(ctr).getIndex(), level--));
 
                 }else if(program.get(ctr).getCode().get(0).getToken().equals("job")){
                       if(level != 0){
@@ -200,7 +200,7 @@ public class ReadFile {
                             || program.get(ctr).getCode().get(0).getToken().equals("do")
                             || program.get(ctr).getCode().get(0).getToken().equals("foreach")) {
 
-                    loopTracker.add(new tuple(program.get(ctr).getIndex(),level), ++level);
+                    loopTracker.add(new tuple(program.get(ctr).getIndex(), ++level));
                 }
 
                 tkStream.clear();
